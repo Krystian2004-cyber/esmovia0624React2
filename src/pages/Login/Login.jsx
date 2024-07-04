ca48d0238896aba1e23410d555907a4fbccd0691
Dave86dev <davidochando1986@gmail.com>
2024-07-04 12:05:25 +0200

    feat: binding done properly

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,46 @@
 
+import { useState, useEffect } from "react";
+import CInput from "../../common/CInput/CInput";
 import "./Login.css";
 
 function Login () {
 
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: ""
+    })
+
+    const inputHandler = (e) => {
+        //Binding process
+        setCredentials((prevState) => ({
+            ...prevState,
+            [e.target.name] : e.target.value
+            //email : maciej@gmail.com
+        }))
+    }
+
+    useEffect(()=>{
+
+        console.log(credentials)
+
+    }, [credentials])
+
     return (
         <div className="login-design">
-            I am loginnnnnnnnn, can you believe it?
+            <CInput 
+                type="email"
+                name="email"
+                placeholder=""
+                design="basic-input"
+                emitFunction={inputHandler}
+            />
+            <CInput 
+                type="password"
+                name="password"
+                placeholder=""
+                design="basic-input"
+                emitFunction={inputHandler}
+            />
         </div>
     )
 }
