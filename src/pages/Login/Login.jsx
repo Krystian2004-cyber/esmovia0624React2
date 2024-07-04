f4fb2bc02c500c20b966636ec3861d15bafe57ca
Dave86dev <davidochando1986@gmail.com>
2024-07-04 10:22:34 +0200

    feat: basic spa app

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+
+import "./Login.css";
+
+function Login () {
+
+    return (
+        <div className="login-design">
+            I am loginnnnnnnnn, can you believe it?
+        </div>
+    )
+}
+
+export default Login;
