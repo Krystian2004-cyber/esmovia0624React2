62714e3bf3b8b00770afbe68607ef06db36891cb
Dave86dev <davidochando1986@gmail.com>
2024-07-04 13:20:20 +0200

    feat: some little refactor

--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -1,7 +1,9 @@
 
+const root = "https://dummyjson.com/"
+
 export async function LoginMe (credentials) {
 
-    let rawData = await fetch('https://dummyjson.com/auth/login', {
+    let rawData = await fetch(`${root}auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
