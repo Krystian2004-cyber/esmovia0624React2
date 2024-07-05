86de5142165aa5d1d4a943c230a48af740b927be
Dave86dev <davidochando1986@gmail.com>
2024-07-05 12:00:35 +0200

    feat: themoviedb api call

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,42 @@
-
+import { useState } from "react";
 import "./Home.css";
+import { useEffect } from "react";
+import { bringMovies } from "../../services/api-calls";
 
-function Home () {
+function Home() {
+  const [movies, setMovies] = useState([]);
 
-    return (
-        <div className="home-design">
-            I am home, can you believe it?
+  useEffect(() => {
+    if (movies.length === 0) {
+      const getMovies = async () => {
+        bringMovies()
+          .then((res) => {
+            setMovies(res.results);
+          })
+          .catch((error) => console.log(error));
+      };
+      setTimeout(() => {
+        getMovies();
+      }, 2000);
+    }
+
+    console.log(movies);
+  }, [movies]);
+
+  return (
+    <div className="home-design">
+      {movies.length > 0 ? (
+        //I have got the movies
+        <div>
+          {movies.map((movie) => {
+            return <div key={movie.id}>{movie.title}</div>;
+          })}
         </div>
-    )
+      ) : (
+        <div>LOADING.......</div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
